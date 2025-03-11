90c33b551b7697d95c62e714d651dc3c175d650c
user <a76650051@gmail.com>
2025-03-11 21:47:10 +0900

    feat: 20250311 react essential project started

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,16 +1,39 @@
+// import Header from "./components/Header";
+
+const reactDescriptionsKor = ['근본적인', '결정적인', '핵심적인']
+const reactDescriptionsEng = ['Fundamental', 'Crucial' , 'Core']
+
+function getRandomInt(max){
+  return Math.floor(Math.random()*(max + 1)); // return 값은 0~2 의 int 값
+}
+console.log(getRandomInt(2))
+
+
+
+function Header(){
+  const randomInt = getRandomInt(2)
+  return( 
+    <header>
+    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <h1>React 필수 요소 복습</h1>
+    <p>
+      {reactDescriptionsEng[randomInt]} React concepts you will need for almost any app you are
+      going to build!
+      <br />
+      대부분의 앱을 위해 당신이 필요로 할 {reactDescriptionsKor[randomInt]} React 개념😶
+    </p>
+    </header>
+    );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
+        <br />
+        <h2>시작</h2>
       </main>
     </div>
   );
---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+  return( 
+  <header>
+  <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+  <h1>React 필수 요소 복습</h1>
+  <p>
+    Fundamental React concepts you will need for almost any app you are
+    going to build!
+    <br />
+    대부분의 앱을 위해 당신이 필요로 할 근본적인 React 개념😶
+  </p>
+  </header>
+  );
+}
